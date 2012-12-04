50c2bb2998221adbadf7dcd206fa04bacb20739c
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2012-12-04 20:38:19 +0100

    networks hint centered and below networks box

--- home.php
+++ home.php
@@ -10,15 +10,16 @@
 		<p>
 			This is where I'm going to list all the little projects I've been working on. You can also take a look at my <a href="./?p=resume">resume</a>.
 		</p>
-		<p>
-			Feel free to check some of my social networks above.
-		</p>
-        <div id="networks">
+		<div id="networks">
 			<div id="networksbox">
 				<a href="https://twitter.com/#!/amasciul"><img src="./img/twitter.png" alt="Twitter" /></a>
 				<a href="https://plus.google.com/104609621580931327067/posts"><img src="./img/plus.png" alt="Google Plus" /></a>
 				<a href="http://www.linkedin.com/pub/alexandre-masciulli/51/567/b00"><img src="./img/linkedin.png" alt="LinkedIn" /></a>
 			</div>
-        </div>
+			 <p>
+				Feel free to check some of my social networks.
+			</p>
+		</div>
+
 	</article>
 </section>
