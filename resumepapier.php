e19fa0149d77bf23ca327c4ff7c3cd2710ca92b6
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-03-30 17:50:52 +0100

    add resume paper version

---
+++ resumepapier.php
@@ -0,0 +1,140 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	    <head>
+        <meta charset="utf-8" />
+        <title>Alexandre Masciulli</title>
+        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:regular,medium,thin,italic,mediumitalic,bold" title="roboto"/>
+        <link href="./designpapier.css" rel="stylesheet">
+		<link rel="stylesheet" href="slimbox/css/slimbox.css" type="text/css" media="screen" />
+		<link rel="icon" type="image/png" href="img/favicon.png" />
+
+        <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+          <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+    </head>
+	
+	<body>
+		<div id="page">
+			<header>
+				<div id="maintitle">
+					<h1><a href="./?p=home" title="Home" rel="home">Alexandre Masciulli</a></h1>
+				</div>
+				<nav>
+				</nav>
+			</header>
+			<div id="box">
+				<section id="contact">
+	<div class="contactbox">
+		<ul>
+			<li>Computer Science and<br>Software Engineering<br>student</li>
+			<li>Université de Technologie de Compiègne</li>
+		</ul>
+	</div>
+	<div id="contactspace">
+	</div>
+	<div class="contactbox">
+		<ul>
+			<li>4 bis rue Jean Mermoz</li>
+			<li>95120 ERMONT - France</li>
+			<li>+33 6 48 03 12 46</li>
+			<li>alexandre.masciulli@gmail.com</li>
+			<li>masciulli.fr</li>
+		</ul>
+	</div>
+</section>
+<section id="aim">
+	<strong>Aim : </strong>6-month full-time internship (last step before graduation) starting February 2014
+</section>
+<section class="categorie">
+	<article class="noaside">
+		<h1>Software engineering experience</h1>
+		<ul>
+			<li>
+				<span class="date">Mobile apps development</span>
+				<div class="description">Designed several Android responsive apps. Also worked on Windows 8 app conception.</div>
+			</li>
+			<li>
+				<span class="date">Client and server-side web development</span>
+				<div class="description">Designing skills with web application frameworks (Symfony 2).</div>
+			</li>
+			<li>
+				<span class="date">Computer science skills</span>
+				<div class="description">Experience in software conception, algorithms, data structures.</div>
+			</li>
+			<li>
+				<span class="date">Technical skills</span>
+				<div class="description">Strong experience in Java, C#.NET, C++, PHP, SQL and Javascript.</div>
+			</li>
+		</ul>
+	</article>
+</section>
+<section class="categorie">
+	<article class="noaside">
+		<h1>Work experience</h1>
+		<ul>
+			<li>
+				<span class="date">September 2012 to February 2013</span> : Shopmium, <span class="lieu">Paris, France</span>
+				<div class="description">Six-month internship as a cross-platform mobile software developer.</div>
+			</li>
+			<li>
+				<span class="date">August 2010 and August 2011</span> : LCL (French bank), <span class="lieu">Neuilly-Sur-Seine, France</span>
+				<div class="description">Summer job in a bank (provided basic customer service).</div>
+			</li>
+			<li>
+				<span class="date">February 2010</span> : STPI, <span class="lieu">Paris, France</span>
+				<div class="description">One-month work placement in a electric components factory (explored the different jobs and experienced various positions).</div>
+			</li>
+		</ul>
+	</article>
+</section>
+<section class="categorie">
+	<article class="noaside">
+		<h1>Diplomas and Education</h1>
+		<h4>Diplomas</h4>
+		<ul>
+			<li><span class="date">Expected 2014</span> : Diplôme d'Ingénieur en Génie Informatique (Master's in Computer Engineering)</li>
+			<li><span class="date">2009</span> : Baccalauréat (final French High School diploma) with emphasis on Maths and Science (with distinctions).</li>
+		</ul>
+		<h4>Education since High School</h4>
+		<ul>
+			<li>
+				<span class="date">2011 - present</span> : Université de Technologie de Compiègne, <span class="lieu">Compiègne, France</span> :
+				<div class="description">Engineering studies in Computer Science Department (expected graduation June 2014).</div>
+			</li>
+			<li>
+				<span class="date">2009 - 2011</span> : Université de Technologie de Compiègne, <span class="lieu">Compiègne, France</span> :
+				<div class="description">General engineering studies : maths, mechanics, physics, computer engineering.</div>
+			</li>
+			<li>
+				<span class="date">2006 - 2009</span> : Lycée (French High School) Van Gogh, <span class="lieu">Ermont, France</span> :
+				<div class="description">Graduated June 2009.</div>
+			</li>
+		</ul>
+	</article>
+</section>
+<section class="categorie">
+	<article class="noaside">
+		<h1>Languages</h1>
+		<ul>
+			<li>
+				<span class="date">English</span> : Proficiency (European C2 level).
+			</li>
+			<li>
+				<span class="date">Spanish</span> : Upper intermediate (European B2 Level).
+			</li>
+			<li>
+				<span class="date">German</span> : Notions (European A1 level).
+			</li>
+		</ul>
+	</article>
+</section>
+
+			</div>
+		</div>
+	
+	</body>
+	
+</html>
